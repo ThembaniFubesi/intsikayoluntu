f2b3c6b2846992cda78ffcf4fe6874717a0617cb
Simo Mafuxwana <simom@hotmail.co.za>
2014-05-11 17:50:28 +0200

    add footer

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -10,3 +10,10 @@
     <a href="<?= getServerUrl(); ?>/pages/gallery.php" class="gallery trans-all">Gallery</a>
     <a href="<?= getServerUrl(); ?>/pages/contact.php" class="contact trans-all">Contact</a>
 </nav>
+<footer>
+	<span class="instika-copyright">Instika Yoluntu Development Project</span><br/>
+	 &copy; 2014 All rights reserved.<br/><br/>
+	<span class="own-appeal-copyright">made with looove by <br/> 
+		<span class="ownappeal"><a href="http://own-appeal.com">Own Appeal</a></span>
+	</span>
+</footer>
