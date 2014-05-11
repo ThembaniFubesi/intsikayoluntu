ea6893a1e76eec0f8e3f24ad2b484b120c028577
Simo Mafuxwana <simom@hotmail.co.za>
2014-05-11 17:57:12 +0200

    Rename Activites to Home

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -4,7 +4,7 @@
     </a>
 </div>
 <nav class="centerDiv">
-    <a href="<?= getServerUrl(); ?>/pages/activities.php" class="activities trans-all">Activities</a>
+    <a href="<?= getServerUrl(); ?>/pages/activities.php" class="activities trans-all">Home</a>
     <a href="<?= getServerUrl(); ?>/pages/values.php" class="values trans-all">Values</a>
     <a href="<?= getServerUrl(); ?>/pages/history.php" class="history trans-all">History</a>
     <a href="<?= getServerUrl(); ?>/pages/gallery.php" class="gallery trans-all">Gallery</a>
